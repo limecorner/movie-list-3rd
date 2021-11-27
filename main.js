f723e0e456160d904c24a7da0c1aa5989e9dbcac
Limecorner <iversonwang40@gmail.com>
2021-11-27 22:55:35 +0800

    Click 'More' can show Modal

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const BASE_URL = 'https://movie-list.alphacamp.io/api/v1/'
 const ALL_MOVIES_URL = BASE_URL + 'movies'
+const SHOW_MOVIE_URL = BASE_URL + 'movies/'
+const POSTER_URL = 'https://movie-list.alphacamp.io/posters/'
 const movieList = document.querySelector('#movie-list')
 
-function renderMovieCard(movies) {
+function renderMoviesCardForm(movies) {
   let rawHTML = ''
   movies.forEach(movie => {
     rawHTML += `<div class="card m-2" style="width: 22%;">
-      <img src="https://movie-list.alphacamp.io/posters/${movie.image}" class="card-img-top" alt="...">
+      <img src="${POSTER_URL}${movie.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${movie.title}</h5>
       </div>
       <div class="card-footer text-muted">
-        <button class="btn btn-primary">More</button>
+        <button class="more btn btn-primary" data-bs-toggle="modal" data-bs-target="#movieModal"  data-id="${movie.id}">More</button>
         <button class="btn btn-info">+</button>
       </div>
     </div>`
@@ -19,13 +21,40 @@ function renderMovieCard(movies) {
   })
 }
 
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('.modal-title')
+  const moviePoster = document.querySelector('.modal-body img')
+  const movieDescription = document.querySelector('.modal-body #movie-description')
+  const movieReleaseDate = document.querySelector('.modal-body #movie-release-date')
+  axios.get(SHOW_MOVIE_URL + id)
+    .then(function (response) {
+      // handle success
+      const movie = response.data.results
+      modalTitle.innerText = movie.title
+      moviePoster.src = POSTER_URL + movie.image
+      movieReleaseDate.innerText = `release date: ${movie.release_date}`
+      movieDescription.innerText = movie.description
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+}
+
 axios.get(ALL_MOVIES_URL)
   .then(function (response) {
     // handle success
     const movies = response.data.results
-    renderMovieCard(movies)
+    renderMoviesCardForm(movies)
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   })
+
+movieList.addEventListener('click', e => {
+  if (e.target.matches('.more')) {
+    const id = e.target.dataset.id
+    showMovieModal(id)
+  }
+})

4454fdc93e2ed6816e6ad70f2f1eacad567383e4
Limecorner <iversonwang40@gmail.com>
2021-11-27 16:53:02 +0800

    initialize view with movie data

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
+const BASE_URL = 'https://movie-list.alphacamp.io/api/v1/'
+const ALL_MOVIES_URL = BASE_URL + 'movies'
 const movieList = document.querySelector('#movie-list')
 
-function renderMovieCard() {
+function renderMovieCard(movies) {
   let rawHTML = ''
-
-  for (let i = 0; i < 12; i++) {
+  movies.forEach(movie => {
     rawHTML += `<div class="card m-2" style="width: 22%;">
-      <img src="https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg" class="card-img-top" alt="...">
+      <img src="https://movie-list.alphacamp.io/posters/${movie.image}" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">AntMan</h5>
+        <h5 class="card-title">${movie.title}</h5>
       </div>
       <div class="card-footer text-muted">
         <button class="btn btn-primary">More</button>
@@ -15,6 +16,16 @@ function renderMovieCard() {
       </div>
     </div>`
     movieList.innerHTML = rawHTML
-  }
+  })
 }
-renderMovieCard()
+
+axios.get(ALL_MOVIES_URL)
+  .then(function (response) {
+    // handle success
+    const movies = response.data.results
+    renderMovieCard(movies)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
